a4311594ee0725e336310fc786978b09ca7b7ba7
N-eeraj <neeraj7rajeena@gmail.com>
2026-01-21 23:46:15 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link"
+
+const RENDERING_OPTIONS = [
+  {
+    label: "Static SSG (static content only)",
+    url: "/ssg/static",
+  },
+  {
+    label: "SSG with build-time fetch",
+    url: "/ssg/build-time-fetch",
+  },
+  {
+    label: "ISR",
+    url: "/isr",
+  },
+  {
+    label: "SSR",
+    url: "/ssr",
+  },
+  {
+    label: "Streaming",
+    url: "/streaming",
+  },
+  {
+    label: "PPR (Partial Prerendering)",
+    url: "/ppr",
+  },
+]
+
+function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl md:text-3xl font-semibold">
+        React Rendering Demo
+      </h1>
+
+      <ol className="ml-5 list-decimal">
+        {RENDERING_OPTIONS.map(({ label, url }, index) => (
+          <li key={index}>
+            <Link
+              href={url}
+              className="hover:text-sky-400 hover:underline">
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ol>
+    </main>
+  )
+}
+
+export default Home
